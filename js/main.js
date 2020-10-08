c8337871383a0a92b6ee81dbe024c649ce06cb44
Manuel Guevara <manuel.guevara-garban@etu.u-bordeaux.fr>
2020-10-08 15:01:18 +0200

    New font added some text changed

--- js/main.js
+++ js/main.js
@@ -36,7 +36,8 @@
   var carousel = function() {
 		$('.home-slider').owlCarousel({
 	    loop:true,
-	    autoplay: true,
+		autoplay: true,
+		autoplayTimeout:8000,
 	    margin:0,
 	    animateOut: 'fadeOut',
 	    animateIn: 'fadeIn',
